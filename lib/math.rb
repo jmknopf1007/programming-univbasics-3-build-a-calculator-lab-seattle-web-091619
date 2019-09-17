126f0b0bd2683f3dfad58c58efe72fe7b8afe741
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-17 16:31:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,14 +4,17 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  return difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
+  
 
 end
 
